d03a17cf7be64a5e9bc1f13c8ccb5a040809f95b
shanmuga <shanmukhasri00@gmail.com>
2026-03-23 17:02:55 +0530

    clear comment lines and add extra comment in approches

--- src/components/clocklist/clockview.js
+++ src/components/clocklist/clockview.js
@@ -1,7 +1,10 @@
 import { Component } from "react";
+
 import "./clockview.css";
 
 class Clock extends Component {
+  /*----------------------- ----------------------------------------------------------------------------------*/
+
   /*  -------------OLD APPROACH --------------------
   constructor(props) {
     super(props)
@@ -10,25 +13,21 @@ class Clock extends Component {
 
   /* -------------------- MODERN APPROCH ----------- */
   state = { date: new Date() };
-
+  /*----------------------- ------------------------------------------------------------------------------------*/
   componentWillUnmount() {
-    // console.log("Unmount called");
     clearInterval(this.timerId);
   }
 
   tick = () => {
     this.setState({ date: new Date() });
-    // console.log("tick has updated");
   };
 
   componentDidMount() {
-    // console.log("Did mount called");
     this.timerId = setInterval(this.tick, 1000);
   }
 
   render() {
     const { date } = this.state;
-    // console.log("render called", date);
     return (
       <div className="clock-container">
         <h1 className="heading">Clock</h1>
